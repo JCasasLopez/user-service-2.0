231d2765fef1a7e28d88ec60bd8ff6c1acdc80d4
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-21 20:20:54 +0100

    First commit and add User entity

---
+++ src/main/java/dev/jcasaslopez/user/entity/User.java
@@ -0,0 +1,123 @@
+package dev.jcasaslopez.user.entity;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
+		private int idUser;
+		
+		@Column(unique=true)
+		private String username;
+		
+		private String password;
+		private String fullName;
+		
+		@Column(unique=true)
+		private String email;
+		private LocalDate dateOfBirth;
+		
+		@ManyToMany(fetch = FetchType.EAGER) 
+	    @JoinTable(name="user_roles",
+	        joinColumns=@JoinColumn(name="user_id", referencedColumnName="idUser"),
+	        inverseJoinColumns=@JoinColumn(name="rol_id", referencedColumnName="idRole"))
+		private Set<Role> roles;
+		
+		@OneToMany
+		@JoinColumn(name="idUser", referencedColumnName="idUser")
+		private Set<LoginAttempt> loginAttempts;
+
+		public User(int idUser, String username, String password, String fullName, String email,
+				LocalDate dateOfBirth) {
+			this.idUser = idUser;
+			this.username = username;
+			this.password = password;
+			this.fullName = fullName;
+			this.email = email;
+			this.dateOfBirth = dateOfBirth;
+		}
+
+		public User() {
+			super();
+		}
+
+		public int getIdUser() {
+			return idUser;
+		}
+
+		public void setIdUser(int idUser) {
+			this.idUser = idUser;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getFullName() {
+			return fullName;
+		}
+
+		public void setFullName(String fullName) {
+			this.fullName = fullName;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public LocalDate getDateOfBirth() {
+			return dateOfBirth;
+		}
+
+		public void setDateOfBirth(LocalDate dateOfBirth) {
+			this.dateOfBirth = dateOfBirth;
+		}
+
+		public Set<Role> getRoles() {
+			return roles;
+		}
+
+		public void setRoles(Set<Role> roles) {
+			this.roles = roles;
+		}
+
+		public Set<LoginAttempt> getLoginAttempts() {
+			return loginAttempts;
+		}
+
+		public void setLoginAttempts(Set<LoginAttempt> loginAttempts) {
+			this.loginAttempts = loginAttempts;
+		}
+}
