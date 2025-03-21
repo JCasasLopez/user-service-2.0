6d20cdcd6360b497d3c29bb43450d34a170f942b
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-21 21:16:46 +0100

    Feat: add Role entity

---
+++ src/main/java/dev/jcasaslopez/user/entity/Role.java
@@ -0,0 +1,64 @@
+package dev.jcasaslopez.user.entity;
+
+import java.util.Set;
+
+import dev.jcasaslopez.user.enums.RoleName;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Table(name="roles")
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idRole;
+	
+	@Column(unique=true)
+	@Enumerated(EnumType.STRING)
+	private RoleName roleName;
+	
+	@ManyToMany(mappedBy="roles")
+	private Set<User> users;
+
+	public Role(int idRole, RoleName roleName) {
+		this.idRole = idRole;
+		this.roleName = roleName;
+	}
+
+	public Role() {
+		super();
+	}
+
+	public int getIdRole() {
+		return idRole;
+	}
+
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+
+	public RoleName getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(RoleName roleName) {
+		this.roleName = roleName;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+
+}
