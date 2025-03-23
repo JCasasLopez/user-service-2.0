0cc9e8fcdff397975cfd4ad59c47481cfa9e21f7
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-23 13:13:15 +0100

    Test: add tests to validate User entity unicity restrictions and rename original file

---
+++ src/test/java/dev/jcasaslopez/user/entity/UnicityEntityTest.java
@@ -0,0 +1,75 @@
+package dev.jcasaslopez.user.entity;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import dev.jcasaslopez.user.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+
+@DataJpaTest
+class UnicityEntityTest {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private EntityManager entityManager;
+	
+	@BeforeEach
+	private void setUp() {
+		User user1 = new User(
+			    0,
+			    "Johnny",
+			    "securePassword123",
+			    "John Doe",
+			    "123@example.com",
+			    LocalDate.of(1990, 5, 15)
+			);
+		
+		userRepository.save(user1);
+		entityManager.flush();
+	}
+
+	@Test
+	@DisplayName("User entity throws exception when 2 users have the same username")
+	void userEntity_WhenUsernameUnicityViolated_ShouldThrowException() {
+		// Arrange
+		User user2 = new User(
+			    0,
+			    "Johnny",
+			    "anotherPassword456",
+			    "Jane Doe",
+			    "laura92@example.com",
+			    LocalDate.of(1992, 8, 22)
+			);
+		
+		// Act & Assert
+		assertThrows(DataIntegrityViolationException.class, () -> userRepository.save(user2),
+				"DataIntegrityViolationException was expected, but was not thrown");
+	}
+	
+	@Test
+	@DisplayName("User entity throws exception when 2 users have the same email")
+	void userEntity_WhenEmailUnicityViolated_ShouldThrowException() {
+		// Arrange
+		User user2 = new User(0, 
+				"Laura", 
+				"anotherPassword456", 
+				"Jane Doe", 
+				"123@example.com",
+				LocalDate.of(1992, 8, 22));
+
+		// Act & Assert
+		assertThrows(DataIntegrityViolationException.class, () -> userRepository.save(user2),
+				"DataIntegrityViolationException was expected, but was not thrown");
+	}
+
+}
