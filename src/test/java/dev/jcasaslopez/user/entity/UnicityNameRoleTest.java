a89965d8f9552704c619d76257bd431bec46e62b
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-23 20:02:34 +0100

    Test: verify uniqueness of Role field 'name'

---
+++ src/test/java/dev/jcasaslopez/user/entity/UnicityNameRoleTest.java
@@ -0,0 +1,32 @@
+package dev.jcasaslopez.user.entity;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import dev.jcasaslopez.user.enums.RoleName;
+import dev.jcasaslopez.user.repository.RoleRepository;
+
+@DataJpaTest
+public class UnicityNameRoleTest {
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Test
+	@DisplayName("Role entity throws exception when 2 roles have the same name")
+	void roleEntity_WhenNameUnicityViolated_ShouldThrowException() {
+		Role role1 = new Role(RoleName.ROLE_USER);
+	    roleRepository.save(role1);
+
+	    Role role2 = new Role(RoleName.ROLE_USER); 
+
+	    assertThrows(DataIntegrityViolationException.class, () -> {
+	        roleRepository.save(role2);
+	    });
+	}
+}
