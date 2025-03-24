cca58bbe06bebc7fb2d16bee189da98b45dc2578
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-24 19:50:44 +0100

    Fix(LoginAttemptMapper): update methods to match new field 'user' class in LoginAttemptDto and rename class to fix typo

--- src/main/java/dev/jcasaslopez/user/mapper/LoginAttemptMapper.java
+++ src/main/java/dev/jcasaslopez/user/mapper/LoginAttemptMapper.java
@@ -1,12 +1,16 @@
 package dev.jcasaslopez.user.mapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import dev.jcasaslopez.user.dto.LoginAttemptDto;
 import dev.jcasaslopez.user.entity.LoginAttempt;
 
 @Component
-public class LoginAttempMapper {
+public class LoginAttemptMapper {
+	
+	@Autowired
+	private UserMapper userMapper;
 	
 	public LoginAttemptDto LoginAttempToDtoMapper (LoginAttempt loginAttempt) {
 		return new LoginAttemptDto(loginAttempt.getIdLoginAttempt(),
@@ -14,7 +18,7 @@ public class LoginAttempMapper {
 				loginAttempt.isSuccessful(),
 				loginAttempt.getIpAddress(),
 				loginAttempt.getLoginFailureReason(),
-				loginAttempt.getUser());
+				userMapper.userToUserDtoMapper(loginAttempt.getUser()));
 	}
 	
 	public LoginAttempt LoginAttempDtoToLoginAttemptMapper (LoginAttemptDto loginAttempt) {
