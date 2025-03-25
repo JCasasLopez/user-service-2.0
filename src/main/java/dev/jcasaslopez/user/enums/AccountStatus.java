0807ff3e1fc1cc343b36306a178f477cacf05df8
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-25 11:17:19 +0100

    Refactor(AccountStatus): change enumeration elements names for clarity and add comments

--- src/main/java/dev/jcasaslopez/user/enums/AccountStatus.java
+++ src/main/java/dev/jcasaslopez/user/enums/AccountStatus.java
@@ -1,7 +1,23 @@
 package dev.jcasaslopez.user.enums;
 
 public enum AccountStatus {
-	ACTIVE,
-	BLOCKED,
-	SUSPENDED
+	
+    // Cuenta completamente operativa
+    // Account is fully operational
+    ACTIVE,
+
+    // Cuenta bloqueada temporalmente por razones administrativas o de seguridad.
+    // Puede ser reactivada posteriormente.
+    //
+    // Account is temporarily blocked due to administrative or security reasons.
+    // It may be reactivated later.
+    TEMPORARILY_BLOCKED,
+
+    // Cuenta suspendida permanentemente. No puede ser reactivada.
+    // Esto usualmente implica la eliminación o desactivación de los datos asociados.
+    //
+    // Account is permanently suspended and cannot be reactivated.
+    // This typically results in data removal or deactivation.
+    PERMANENTLY_SUSPENDED
 }
+
