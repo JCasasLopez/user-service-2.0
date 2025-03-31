0b0d545a6de91470b208059baeb37a6707fd4fac
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-31 19:20:19 +0200

    Chore(PasswordServiceImpl): fix logger class reference

--- src/main/java/dev/jcasaslopez/user/service/PasswordServiceImpl.java
+++ src/main/java/dev/jcasaslopez/user/service/PasswordServiceImpl.java
@@ -20,7 +20,7 @@ import dev.jcasaslopez.user.repository.UserRepository;
 @Service
 public class PasswordServiceImpl implements PasswordService {
 	
-	private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsManagerImpl.class);
+	private static final Logger logger = LoggerFactory.getLogger(PasswordServiceImpl.class);
 	// Requisitos: Al menos 8 caracteres, una letra mayúscula, una minúscula, un número y un símbolo
 	//
 	// requirements: At least 8 characters, one capital letter, one lowercase letter, one number and one symbol.
