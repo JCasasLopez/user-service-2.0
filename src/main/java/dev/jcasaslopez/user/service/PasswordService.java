e14229b91f30e87b0c64c5b66c7298d80da4e59f
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-01 14:08:06 +0200

    Docs(PasswordService): remove redundant comment from interface, kept in implementation

--- src/main/java/dev/jcasaslopez/user/service/PasswordService.java
+++ src/main/java/dev/jcasaslopez/user/service/PasswordService.java
@@ -3,16 +3,7 @@ package dev.jcasaslopez.user.service;
 public interface PasswordService {
 
 	void changePassword(String oldPassword, String newPassword);
-
-	// Aunque resetPassword() y changePassword() son superficialmente similares,
-	// este último opera con el usuario ya autenticado. Además, sus firmas son distintas:
-	// resetPassword() no necesita verificar si la contraseña antigua coincide.
-	//
-	// While resetPassword() and changePassword() are superficially similar, the
-	// latter operates with the user already authenticated. Also, their method signatures differ:
-	// resetPassword() does not need to verify if the old password matches.
 	void resetPassword(String newPassword, String token);
-
 	boolean passwordIsValid(String newPassword);
 
 }
