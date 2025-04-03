954853937b907b1a266a19db170c0c6063761a88
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-03 18:47:17 +0200

    Chore(LoginFailureReason): add 'MISSING_FIELD' to possible failure reasons

--- src/main/java/dev/jcasaslopez/user/enums/LoginFailureReason.java
+++ src/main/java/dev/jcasaslopez/user/enums/LoginFailureReason.java
@@ -3,6 +3,7 @@ package dev.jcasaslopez.user.enums;
 public enum LoginFailureReason {
 	INCORRECT_PASSWORD,
     USER_NOT_FOUND,
+    MISSING_FIELD,
     ACCOUNT_LOCKED,
     TOKEN_EXPIRED,
     TOKEN_MALFORMED,
