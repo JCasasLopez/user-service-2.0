6b56d6220f96fab617256282563c289baa1a8886
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-07 13:07:02 +0200

    Feat(TokenType): add prefix field and constructor to support string representation

--- src/main/java/dev/jcasaslopez/user/enums/TokenType.java
+++ src/main/java/dev/jcasaslopez/user/enums/TokenType.java
@@ -1,7 +1,17 @@
 package dev.jcasaslopez.user.enums;
 
 public enum TokenType {
-	VERIFICATION,
-    ACCESS,
-    REFRESH
+    VERIFICATION("verification"),
+    ACCESS("access"),
+    REFRESH("refresh");
+
+    private final String prefix;
+
+    TokenType(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String prefix() {
+        return prefix;
+    }
 }
