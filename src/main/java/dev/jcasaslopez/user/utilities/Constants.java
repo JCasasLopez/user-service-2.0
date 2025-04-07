0033e0d2667057544f187f8782f59e68bd727304
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-07 18:19:30 +0200

    Chore: add endpoint paths to Constants

--- src/main/java/dev/jcasaslopez/user/utilities/Constants.java
+++ src/main/java/dev/jcasaslopez/user/utilities/Constants.java
@@ -3,4 +3,8 @@ package dev.jcasaslopez.user.utilities;
 public class Constants {
 	public static final String LOGIN_ATTEMPTS_REDIS_KEY = "login_attempts:";
 	public static final String REFRESH_TOKEN_REDIS_KEY = "refresh_token:";
+	public static final String LOGOUT_PATH = "/logout";
+	public static final String REFRESH_TOKEN_PATH = "/refreshToken";
+	public static final String REGISTRATION_PATH = "/initiateUserRegistration";
+	public static final String FORGOT_PASSWORD_PATH = "/forgotPassword";
 }
