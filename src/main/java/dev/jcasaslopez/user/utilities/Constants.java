cf709ce32a5fa1c96286244cb3c1c7973987db49
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-07 16:22:37 +0200

    Chore: add constants for Redis keys

---
+++ src/main/java/dev/jcasaslopez/user/utilities/Constants.java
@@ -0,0 +1,6 @@
+package dev.jcasaslopez.user.utilities;
+
+public class Constants {
+	public static final String LOGIN_ATTEMPTS_REDIS_KEY = "login_attempts";
+	public static final String REFRESH_TOKEN_REDIS_KEY = "refresh_token";
+}
