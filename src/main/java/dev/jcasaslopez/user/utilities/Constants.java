5c6ca8ae14ee7144b2fd7c940a9f785263ac7c29
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-08 18:09:49 +0200

    Chore(Constants): add constants representing endpoint paths

--- src/main/java/dev/jcasaslopez/user/utilities/Constants.java
+++ src/main/java/dev/jcasaslopez/user/utilities/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
 	
 	public static final String LOGOUT_PATH = "/logout";
 	public static final String REFRESH_TOKEN_PATH = "/refreshToken";
-	public static final String REGISTRATION_PATH = "/initiateUserRegistration";
+	public static final String INITIATE_REGISTRATION_PATH = "/initiateUserRegistration";
+	public static final String REGISTRATION_PATH = "/userRegistration";
 	public static final String FORGOT_PASSWORD_PATH = "/forgotPassword";
+	public static final String RESET_PASSWORD_PATH = "/resetPassword";
 }
