608420bbd6b747f6f8610292155f6987a43fa0b0
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-10 12:28:31 +0200

    Feat(RoleRepository): add findByRoleName()

--- src/main/java/dev/jcasaslopez/user/repository/RoleRepository.java
+++ src/main/java/dev/jcasaslopez/user/repository/RoleRepository.java
@@ -1,9 +1,14 @@
 package dev.jcasaslopez.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.jcasaslopez.user.entity.Role;
+import dev.jcasaslopez.user.enums.RoleName;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+	
+	Optional<Role> findByRoleName(RoleName roleName);
 
 }
