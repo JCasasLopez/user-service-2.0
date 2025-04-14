f2e0f430119ea4c2f3bbc0b1fcd5a55c500f80e0
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-14 11:40:50 +0200

    Refactor(Constants): add login endpoint path constant

--- src/main/java/dev/jcasaslopez/user/utilities/Constants.java
+++ src/main/java/dev/jcasaslopez/user/utilities/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
 	public static final String REFRESH_TOKEN_REDIS_KEY = "refresh_token:";
 	public static final String CREATE_ACCOUNT_REDIS_KEY = "create_account:";
 	
+	public static final String LOGIN_PATH = "/login";
 	public static final String LOGOUT_PATH = "/logout";
 	public static final String REFRESH_TOKEN_PATH = "/refreshToken";
 	public static final String INITIATE_REGISTRATION_PATH = "/initiateUserRegistration";
