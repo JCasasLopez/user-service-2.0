4a1fbff35c84dbcdcc7bcc864d70cfb4cb9984c5
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-04-29 17:06:13 +0200

    Refactor(TestHelper): remove authentication logic and introduce createUser()

--- src/test/java/dev/jcasaslopez/user/utilities/TestHelper.java
+++ src/test/java/dev/jcasaslopez/user/utilities/TestHelper.java
@@ -1,29 +1,25 @@
 package dev.jcasaslopez.user.utilities;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import dev.jcasaslopez.user.entity.User;
-import dev.jcasaslopez.user.enums.TokenType;
-import dev.jcasaslopez.user.mapper.UserMapper;
-import dev.jcasaslopez.user.security.CustomUserDetails;
-import dev.jcasaslopez.user.service.TokenServiceImpl;
+import dev.jcasaslopez.user.repository.UserRepository;
 
 @Component
 public class TestHelper {
 	
-	@Autowired UserMapper userMapper;
-	@Autowired TokenServiceImpl tokenServiceImpl;
+	@Autowired private UserRepository userRepository;	
 	
-	public String logUserIn(User user) {
-		CustomUserDetails userDetails = userMapper.userToCustomUserDetailsMapper(user);
-		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(),
-				userDetails.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		return tokenServiceImpl.createAuthToken(TokenType.ACCESS);
+	public User createUser() {
+		// El usuario solo tiene el rol "USER", que se asigna automáticamente.
+		//
+		// User only has the role "USER", which is assigned automatically.
+		User plainUser = new User ("Yorch123", "Jorge22!", "Jorge García", "jorgecasas22@hotmail.com", 
+				LocalDate.of(1978, 11, 26));
+		userRepository.save(plainUser);
+		return plainUser;
 	}
-	
 }
