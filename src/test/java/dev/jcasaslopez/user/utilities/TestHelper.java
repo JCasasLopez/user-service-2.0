adbe130ebb7883e0bb9de1f3e8b9b26f6dcc8278
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-05-07 17:58:24 +0200

    Refactor(TestHelper): create user using UserDetailsManager createUser() instead of JPA's save(), so the user is assigned 'user' role and 'active' account status automatically

--- src/test/java/dev/jcasaslopez/user/utilities/TestHelper.java
+++ src/test/java/dev/jcasaslopez/user/utilities/TestHelper.java
@@ -11,24 +11,22 @@ import org.springframework.stereotype.Component;
 import dev.jcasaslopez.user.entity.User;
 import dev.jcasaslopez.user.enums.TokenType;
 import dev.jcasaslopez.user.mapper.UserMapper;
-import dev.jcasaslopez.user.repository.UserRepository;
 import dev.jcasaslopez.user.security.CustomUserDetails;
 import dev.jcasaslopez.user.service.TokenServiceImpl;
+import dev.jcasaslopez.user.service.UserDetailsManagerImpl;
 
 @Component
 public class TestHelper {
 	
-	@Autowired private UserRepository userRepository;	
 	@Autowired private UserMapper userMapper;
-	@Autowired private TokenServiceImpl tokenServiceImpl;	
+	@Autowired private TokenServiceImpl tokenServiceImpl;
+	@Autowired private UserDetailsManagerImpl userDetailsManagerImpl;
 	
 	public User createUser() {
-		// El usuario solo tiene el rol "USER", que se asigna automáticamente.
-		//
-		// User only has the role "USER", which is assigned automatically.
 		User plainUser = new User ("Yorch123", "Jorge22!", "Jorge García", "jorgecasas22@hotmail.com", 
 				LocalDate.of(1978, 11, 26));
-		userRepository.save(plainUser);
+		CustomUserDetails user = userMapper.userToCustomUserDetailsMapper(plainUser);
+		userDetailsManagerImpl.createUser(user);
 		return plainUser;
 	}
 	
