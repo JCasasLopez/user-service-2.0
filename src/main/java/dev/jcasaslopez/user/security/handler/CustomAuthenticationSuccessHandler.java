ffb3a80e0924340543ef9ecd6600ead6da00be99
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-05-13 17:48:32 +0200

    Refactor(CustomAuthenticationSuccessHandler): remove unnecessary Redis token upload and dependencies and redundant comments

--- src/main/java/dev/jcasaslopez/user/security/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/dev/jcasaslopez/user/security/handler/CustomAuthenticationSuccessHandler.java
@@ -2,7 +2,6 @@ package dev.jcasaslopez.user.security.handler;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 import dev.jcasaslopez.user.entity.User;
 import dev.jcasaslopez.user.enums.TokenType;
 import dev.jcasaslopez.user.handler.StandardResponseHandler;
-import dev.jcasaslopez.user.model.TokensLifetimes;
 import dev.jcasaslopez.user.service.LoginAttemptService;
 import dev.jcasaslopez.user.service.TokenService;
 import dev.jcasaslopez.user.service.UserAccountService;
@@ -33,17 +31,15 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 	private StandardResponseHandler standardResponseHandler;
 	private LoginAttemptService loginAttemptService;
 	private TokenService tokenService;
-	private TokensLifetimes tokensLifetimes;
 	private UserAccountService userAccountService;
 
 	public CustomAuthenticationSuccessHandler(StringRedisTemplate redisTemplate,
 			StandardResponseHandler standardResponseHandler, LoginAttemptService loginAttemptService,
-			TokenService tokenService, TokensLifetimes tokensLifetimes, UserAccountService userAccountService) {
+			TokenService tokenService, UserAccountService userAccountService) {
 		this.redisTemplate = redisTemplate;
 		this.standardResponseHandler = standardResponseHandler;
 		this.loginAttemptService = loginAttemptService;
 		this.tokenService = tokenService;
-		this.tokensLifetimes = tokensLifetimes;
 		this.userAccountService = userAccountService;
 	}
 
@@ -56,10 +52,6 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 		User user = userAccountService.findUser(username);
 		String redisKey = Constants.LOGIN_ATTEMPTS_REDIS_KEY + username;
-		
-		// Si no existe esta entrada, no hay error.
-		//
-		// If there is no such entry, no error/exception is thrown.
 		redisTemplate.delete(redisKey);
 		
 		loginAttemptService.recordAttempt(true, request.getRemoteAddr(), null, user);
@@ -67,21 +59,8 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 		String refreshToken = tokenService.createAuthToken(TokenType.REFRESH);
 		String accessToken = tokenService.createAuthToken(TokenType.ACCESS);
 		List<String> refreshAndAccessTokens = List.of(refreshToken, accessToken);
-
-		// Sube el token de refresco a Redis -> clave: refresh_token:jti.
-		//
-		// Persists refresh token in Redis -> key: refresh_token:jti.
-		String refreshTokenJti = tokenService.getJtiFromToken(refreshToken);
-		String refreshTokenRedisKey = Constants.REFRESH_TOKEN_REDIS_KEY + refreshTokenJti;
-		int expirationInSeconds = tokensLifetimes.getTokensLifetimes().get(TokenType.REFRESH) * 60;		
-		redisTemplate.opsForValue().set(refreshTokenRedisKey, TokenType.REFRESH.prefix(), 
-				expirationInSeconds, TimeUnit.SECONDS);
-
 		logger.info("Login successful for user '{}'. Attempts reset and login attempt persisted.", username);
 		
-		// Devuelve los tokens en la respuesta.
-		//
-		// Returns tokens in the response.
 		standardResponseHandler.handleResponse(response, 200, "Login attempt successful", 
 				refreshAndAccessTokens);
 	}
