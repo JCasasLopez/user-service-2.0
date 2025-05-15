b110e167d83b7178dc55522ad108c928ffe8a44c
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-05-15 20:15:34 +0200

    Refactor: delete unncessary method findByTimestampBetween()

--- src/main/java/dev/jcasaslopez/user/repository/LoginAttemptRepository.java
+++ src/main/java/dev/jcasaslopez/user/repository/LoginAttemptRepository.java
@@ -8,9 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import dev.jcasaslopez.user.entity.LoginAttempt;
 
 public interface LoginAttemptRepository extends JpaRepository<LoginAttempt, Long> {
-	
-	// Esta consulta únicamente se usa en el test de integración de CustomAuthenticationSuccessHandler
-	//
-	// This query is only used for CustomAuthenticationSuccessHandler integration test.
-	List<LoginAttempt> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
+
 }
