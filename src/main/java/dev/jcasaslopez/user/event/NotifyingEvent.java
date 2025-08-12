fa9c3d1c550d3eff723bd85960e75102ac1adc99
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-08-12 11:59:22 +0200

    Refactor(NotifyingEvent): remove setters and include NotificationType as field in the constructors to pass this information to the listener class

--- src/main/java/dev/jcasaslopez/user/event/NotifyingEvent.java
+++ src/main/java/dev/jcasaslopez/user/event/NotifyingEvent.java
@@ -2,54 +2,51 @@ package dev.jcasaslopez.user.event;
 
 import dev.jcasaslopez.user.entity.User;
 import dev.jcasaslopez.user.enums.AccountStatus;
+import dev.jcasaslopez.user.enums.NotificationType;
 
 public class NotifyingEvent {
 	
 	private User user;
 	private String token;
 	private AccountStatus accountStatus;
+	private NotificationType notificationType;
 	
 	// Overloaded constructors accommodate varying data needs for events:
 	// - User-only events: ChangePassword, CreateAccount, ResetPassword
 	// - User + AccountStatus: UpdateAccountStatus
 	// - User + token: ForgotPassword, VerifyEmail
-
-	public NotifyingEvent(User user) {
+	
+	public NotifyingEvent(User user, NotificationType notificationType) {
 		this.user = user;
+		this.notificationType = notificationType;
 	}
 
-	public NotifyingEvent(User user, AccountStatus accountStatus) {
-		this.user = user;
-		this.accountStatus = accountStatus;
-	}
-
-	public NotifyingEvent(User user, String token) {
+	public NotifyingEvent(User user, String token, NotificationType notificationType) {
 		this.user = user;
 		this.token = token;
+		this.notificationType = notificationType;
+	}
+
+	public NotifyingEvent(User user, AccountStatus accountStatus, NotificationType notificationType) {
+		this.user = user;
+		this.accountStatus = accountStatus;
+		this.notificationType = notificationType;
 	}
 
 	public User getUser() {
 		return user;
 	}
 
-	public void setUser(User user) {
-		this.user = user;
-	}
-
 	public String getToken() {
 		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
 	}
 
 	public AccountStatus getAccountStatus() {
 		return accountStatus;
 	}
 
-	public void setAccountStatus(AccountStatus accountStatus) {
-		this.accountStatus = accountStatus;
+	public NotificationType getNotificationType() {
+		return notificationType;
 	}
 	
 }
