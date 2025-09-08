12470a27b53a5c681fef1984d716ecb7ae43d4c9
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-09-08 11:59:13 +0200

    Refactor(AuthenticationFilter): delegate business logic to service and HTTP request logic to handler, making the filter smaller and aligned with the Single Responsibility Principle

--- src/main/java/dev/jcasaslopez/user/security/filter/AuthenticationFilter.java
+++ src/main/java/dev/jcasaslopez/user/security/filter/AuthenticationFilter.java
@@ -10,9 +10,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import dev.jcasaslopez.user.dto.AuthenticationRequest;
 import dev.jcasaslopez.user.enums.TokenType;
+import dev.jcasaslopez.user.handler.AuthenticationFlowHandler;
 import dev.jcasaslopez.user.handler.StandardResponseHandler;
 import dev.jcasaslopez.user.service.AuthenticationService;
-import dev.jcasaslopez.user.service.TokenService;
 import dev.jcasaslopez.user.utilities.Constants;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -25,14 +25,14 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 	private static final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
 
 	private final StandardResponseHandler standardResponseHandler;
-	private final TokenService tokenService;
 	private final AuthenticationService authService;
+	private final AuthenticationFlowHandler authFlowHandler;
 
-	public AuthenticationFilter(StandardResponseHandler standardResponseHandler, TokenService tokenService,
-			AuthenticationService authService) {
+	public AuthenticationFilter(StandardResponseHandler standardResponseHandler, AuthenticationService authService,
+			AuthenticationFlowHandler authFlowHandler) {
 		this.standardResponseHandler = standardResponseHandler;
-		this.tokenService = tokenService;
 		this.authService = authService;
+		this.authFlowHandler = authFlowHandler;
 	}
 
 	@Override
@@ -47,6 +47,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 		//    header is malformed.
 		// At any rate, we allow the flow to continue through the Security Filter Chain.
 		if(!authService.verifyHeaderIsValid(request)) {
+			logger.warn("Header is empty or invalid. Security Filter Chain continues...");
 			filterChain.doFilter(request, response);
 			return;		
 		}
@@ -54,8 +55,10 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 		Optional<AuthenticationRequest> optionalAuthRequest = authService.parseAuthenticationRequest(request, response);
 		
 		// CASE 2:
+		// Token is present in the request, but it is not valid.
 		if (optionalAuthRequest.isEmpty()) {
-			standardResponseHandler.handleResponse(response, 401, "Token is expired, invalid or malformed", null);
+			logger.warn("Token is expired, malformed or the signature is invalid");
+			standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
 			return;
 		}
 		
@@ -71,33 +74,32 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
 		try {
 			switch(endpointPath) {
-
-			// TokenType.REFRESH: revokes the token in the service and returns.
+			
+			// TokenType.REFRESH
 			case Constants.LOGOUT_PATH:  
-				handleLogOutFlow(response, token, purpose, method);
+				authFlowHandler.handleLogOutFlow(response, token, purpose, method);
 				break;
 
-				// TokenType.VERIFICATION: we set the token as a request attribute, as we will need it for the 
-				// verification process, and allow the flow to continue to the endpoint.
+			// TokenType.VERIFICATION
 			case Constants.REGISTRATION_PATH:
 			case Constants.RESET_PASSWORD_PATH:
-				handleVerificationFlow(request, response, token, purpose, method, path);
-				filterChain.doFilter(request, response);
+				if(authFlowHandler.handleVerificationFlow(request, response, token, purpose, method, path)) {
+					filterChain.doFilter(request, response);
+				}
 				break;
 
-				// TokenType.REFRESH: we authenticate the user, revoke the refresh token and allow 
-				// the flow to continue to the endpoint.
+			// TokenType.REFRESH
 			case Constants.REFRESH_TOKEN_PATH:
-				handleRefreshFlow(request, response, token, purpose, username, method, path);
-				filterChain.doFilter(request, response);
+				if(authFlowHandler.handleRefreshFlow(request, response, token, purpose, username, method, path)) {
+					filterChain.doFilter(request, response);
+				}
 				break;
 
-				// If there is a token and it is valid, but the HTTP request does not match any of the above cases, 
-				// then we are dealing with one of the endpoints that need a TokenType.ACCESS.
-				// We authenticate the user and allow the flow to continue to the endpoint.
+			// TokenType.ACCESS
 			default:
-				handleAccessFlow(response, token, purpose, username);
-				filterChain.doFilter(request, response);
+				if(authFlowHandler.handleAccessFlow(response, token, purpose, username)) {
+					filterChain.doFilter(request, response);
+				}
 				break;
 			}
 
@@ -105,63 +107,5 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 			logger.warn("I/O error while writing HTTP response", ex);
 			standardResponseHandler.handleResponse(response, 500, "An error occurred while processing the response", null);
 		}
-
-	}
-		
-	// Logout
-	// You have to receive the refresh token, not the access one.
-	// No need to check blacklist status - logout achieves the same end result regardless.
-	private void handleLogOutFlow(HttpServletResponse response, String token, TokenType purpose, String method) 
-			throws IOException {
-		
-		if("POST".equalsIgnoreCase(method) && purpose == TokenType.REFRESH) {
-			logger.info("Processing logout request");
-			tokenService.logOut(token);
-			standardResponseHandler.handleResponse(response, 200, "The user has been logged out", null);
-			return;
-		}
-		standardResponseHandler.handleResponse(response, 401, "Invalid log out request", null);
-		return;
-	}
-	
-	// Verification token
-	private void handleVerificationFlow(HttpServletRequest request, HttpServletResponse response, String token, 
-			TokenType purpose, String method, String path) throws IOException {
-		
-		if(purpose == TokenType.VERIFICATION && ("POST".equalsIgnoreCase(method) || "PUT".equalsIgnoreCase(method))) {
-			request.setAttribute("token", token);
-			logger.info("Valid verification token received for path: {}", path);
-			return;
-		}
-		standardResponseHandler.handleResponse(response, 401, "Invalid verification request", null);
-		return;
-	}
-	
-	// Refresh token
-	private void handleRefreshFlow(HttpServletRequest request, HttpServletResponse response, String token, 
-			TokenType purpose, String username, String method, String path) throws IOException {
-		if ("POST".equalsIgnoreCase(method) && !tokenService.isTokenBlacklisted(token) && 
-				purpose == TokenType.REFRESH) {
-			authService.authenticateUser(token, username);
-			// We have to revoke the token, since the system will issue a new one
-			// in the next step (controller -> AccountOrchestrationService).
-			tokenService.blacklistToken(token);
-			return;
-		}
-		standardResponseHandler.handleResponse(response, 401, "Invalid token refresh request", null);
-		return;
-	}
-	
-	// Access token
-	private void handleAccessFlow(HttpServletResponse response, String token, TokenType purpose, String username) 
-			throws IOException {
-		if (purpose == TokenType.ACCESS) {
-			authService.authenticateUser(token, username);	
-			return;
-		}
-		// No need to handle an incorrect token type here; it will be handled later in the filter chain 
-		// by CustomAuthenticationEntryPoint.
-
-	}
-	
+	}	
 }
