1c3a18b271bc941344a72a7ccdb0da5f9a8690e1
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-09-11 12:44:22 +0200

    Fix(auth): adjust authentication flow to handle different endpoint types

--- src/main/java/dev/jcasaslopez/user/handler/AuthenticationFlowHandler.java
+++ src/main/java/dev/jcasaslopez/user/handler/AuthenticationFlowHandler.java
@@ -1,13 +1,14 @@
 package dev.jcasaslopez.user.handler;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import dev.jcasaslopez.user.enums.TokenType;
-import dev.jcasaslopez.user.security.filter.AuthenticationFilter;
+import dev.jcasaslopez.user.service.AccountOrchestrationService;
 import dev.jcasaslopez.user.service.AuthenticationService;
 import dev.jcasaslopez.user.service.TokenService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,17 +17,19 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class AuthenticationFlowHandler {
 	
-	private static final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
+	private static final Logger logger = LoggerFactory.getLogger(AuthenticationFlowHandler.class);
 
 	private final StandardResponseHandler standardResponseHandler;
 	private final TokenService tokenService;
 	private final AuthenticationService authService;
+	private final AccountOrchestrationService accountOrchestrationService;
 	
 	public AuthenticationFlowHandler(StandardResponseHandler standardResponseHandler, TokenService tokenService,
-			AuthenticationService authService) {
+			AuthenticationService authService, AccountOrchestrationService accountOrchestrationService) {
 		this.standardResponseHandler = standardResponseHandler;
 		this.tokenService = tokenService;
 		this.authService = authService;
+		this.accountOrchestrationService = accountOrchestrationService;
 	}
 
 	// Logout
@@ -34,83 +37,61 @@ public class AuthenticationFlowHandler {
 	// No need to check blacklist status - logout achieves the same end result regardless.
 	public void handleLogOutFlow(HttpServletResponse response, String token, TokenType purpose, String method) throws IOException {
 
-		if(!"POST".equalsIgnoreCase(method)) {
-			logger.warn("Expected HTTP method POST, but received {}", method);
-			standardResponseHandler.handleResponse(response, 405, "HTTP method not allowed", null);
-			return;
-		}
-
 		if(purpose != TokenType.REFRESH) {
-			logger.warn("Expected token type REFRESH, but received {}", purpose.name());
+			logger.warn("Expected REFRESH token, but received {}", purpose.name());
 			standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
 			return;
 		}
 		
-		logger.info("Valid refresh token received for logging out path");
+		logger.info("Valid REFRESH token received for logging out path");
 		tokenService.logOut(token);
 		standardResponseHandler.handleResponse(response, 200, "The user has been logged out", null);
-		return;
 	}
 
 	// Verification token
-	public boolean handleVerificationFlow(HttpServletRequest request, HttpServletResponse response, String token, 
-			TokenType purpose, String method, String path) throws IOException {
-
-		if(!"POST".equalsIgnoreCase(method) && !"PUT".equalsIgnoreCase(method)) {
-			logger.warn("Expected HTTP method POST/PUT, but received {}", method);
-			standardResponseHandler.handleResponse(response, 405, "HTTP method not allowed", null);
-			return false;
-		}
+	public void handleVerificationFlow(HttpServletRequest request, HttpServletResponse response, String token, 
+			TokenType purpose, String method, String username, String path) throws IOException {
 
 		if(purpose == TokenType.VERIFICATION) {
 			request.setAttribute("token", token);
 			logger.info("Valid verification token received for path: {}", path);
-			return true;
+			return;
 		}
 
-		logger.warn("Expected token type VERIFICATION, but received {}", purpose.name());
+		logger.warn("Expected VERIFICATION token, but received {}", purpose.name());
 		standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
-		return false;		
 	}
 
 	// Refresh token
-	public boolean handleRefreshFlow(HttpServletRequest request, HttpServletResponse response, String token,
-			TokenType purpose, String username, String method, String path) throws IOException {
+	public void handleRefreshFlow(HttpServletRequest request, HttpServletResponse response, String token,
+			TokenType purpose, String username, String path) throws IOException {
+		
+		boolean isTokenBlacklisted = tokenService.isTokenBlacklisted(token);
 
-		if(!"POST".equalsIgnoreCase(method)) {
-			logger.warn("Expected HTTP method POST, but received {}", method);
-			standardResponseHandler.handleResponse(response, 405, "HTTP method not allowed", null);
-			return false;
+		if(purpose == TokenType.REFRESH && !isTokenBlacklisted) {
+			logger.info("Processing token refresh for user: {}", username);
+			tokenService.blacklistToken(token);
+			List<String> tokens = accountOrchestrationService.refreshToken();
+			standardResponseHandler.handleResponse(response, 201, "New refresh and access tokens sent successfully", 
+					tokens);
+			return;
 		}
-
+		
 		if(purpose != TokenType.REFRESH) {
-			logger.warn("Expected token type REFRESH, but received {}", purpose.name());
-			standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
-			return false;
-		}
-
-		if(tokenService.isTokenBlacklisted(token)) {
+			logger.warn("Expected REFRESH token, but received {}", purpose.name());
+		} else if (isTokenBlacklisted){
 			logger.warn("Refresh token is blacklisted for user: {}", username);
-			standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
-			return false;
 		}
-
-		logger.info("Processing token refresh for user: {}", username);
-		authService.authenticateUser(token, username);
-		tokenService.blacklistToken(token);
-		return true;
+		standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
 	}
 
 	// Access token
-	public boolean handleAccessFlow(HttpServletResponse response, String token, TokenType purpose, String username) throws IOException {
-		if (purpose == TokenType.ACCESS) {
-			authService.authenticateUser(token, username);	
-			return true;
-		}
-		
-		logger.warn("Expected token type ACCESS, but received {}", purpose.name());
-		standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
-		return false;
+	public void handleAccessFlow(HttpServletResponse response, String token, TokenType purpose, String username) throws IOException {
+		if (purpose != TokenType.ACCESS) {
+			logger.warn("Expected ACCESS token, but received {}", purpose.name());
+		    return;
+		  }
+		  authService.authenticateUser(token, username);
 	}
 
 }
--- src/main/java/dev/jcasaslopez/user/security/filter/AuthenticationFilter.java
+++ src/main/java/dev/jcasaslopez/user/security/filter/AuthenticationFilter.java
@@ -2,6 +2,7 @@ package dev.jcasaslopez.user.security.filter;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +20,26 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+// Endpoints are grouped into:
+// a) Public: no authentication or token required.
+// b) Action-token: require a valid token (verification, logout, refresh), but do not need an authenticated user (populate SecurityContext).
+// c) Protected: require both a valid token and authentication.
+//
+// Flow:
+// - Public endpoints → always pass through.
+// - Action-token / Protected endpoints → validate step by step:
+//   1) Header format (AuthenticationService).
+//   2) Token validity (parsed in AuthenticationService).
+//   3) Token type matches the endpoint (AuthenticationFlowHandler).
+//
+// Behavior:
+// - Action-token endpoints: 
+//   • On success → continue in the Security Filter Chain.  
+//   • On failure → return HTTP response here.
+// - Protected endpoints: 
+//   • On success → authenticate user (populate SecurityContext) and continue.  
+//   • On failure → continue without authentication (SecurityContext remains empty), letting Spring Security's AuthenticationEntryPoint reject the request.
+
 @Component
 public class AuthenticationFilter extends OncePerRequestFilter {
 
@@ -27,7 +48,21 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 	private final StandardResponseHandler standardResponseHandler;
 	private final AuthenticationService authService;
 	private final AuthenticationFlowHandler authFlowHandler;
-
+	
+	private static final Set<String> PUBLIC_ENDPOINTS = Set.of(
+			Constants.LOGIN_PATH,
+			Constants.INITIATE_REGISTRATION_PATH,
+			Constants.FORGOT_PASSWORD_PATH
+		  );
+	
+	// Endpoints that require token, but the user does not need to be authenticated (as in SecurityContext populated).
+	private static final Set<String> ACTION_TOKEN_ENDPOINTS = Set.of(
+		      Constants.LOGOUT_PATH,        // TokenType.REFRESH
+		      Constants.REFRESH_TOKEN_PATH, // TokenType.REFRESH
+		      Constants.REGISTRATION_PATH,  // TokenType.VERIFICATION
+		      Constants.RESET_PASSWORD_PATH // TokenType.VERIFICATION
+		  );
+	
 	public AuthenticationFilter(StandardResponseHandler standardResponseHandler, AuthenticationService authService,
 			AuthenticationFlowHandler authFlowHandler) {
 		this.standardResponseHandler = standardResponseHandler;
@@ -38,34 +73,46 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
+				
+		// Public → passthrough
+		boolean isPublicEndpoint = PUBLIC_ENDPOINTS.contains(request.getServletPath());
 		
-		// CASE 1:
-		// There is no token in the header or it is malformed, which means either of 2 things:
-		// 1) The endpoint needs authentication, but it is not provided, so the flow will be captured by 
-		//    CustomAuthenticationEntryPoint, which will return a 401 response.
-		// 2) The endpoint does NOT need authentication, so it does not matter if the token is missing or the
-		//    header is malformed.
-		// At any rate, we allow the flow to continue through the Security Filter Chain.
-		if(!authService.verifyHeaderIsValid(request)) {
-			logger.warn("Header is empty or invalid. Security Filter Chain continues...");
+		if(isPublicEndpoint) {
+			logger.debug("The endpoint is public. Continues with Security Filter Chain...");
+			filterChain.doFilter(request, response);
+			return;	
+		}
+		
+		// Non-public + missing/invalid header
+		boolean isAuthHeaderInvalid = !authService.verifyHeaderIsValid(request);
+		boolean isActionTokenEndpoint = ACTION_TOKEN_ENDPOINTS.contains(request.getServletPath());
+		
+		if(isAuthHeaderInvalid) {
+			if(isActionTokenEndpoint) {
+				standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
+				return;
+			}
+			logger.debug("Invalid or empty header. Protected endpoint. Continues with Security Filter Chain...");
 			filterChain.doFilter(request, response);
 			return;		
 		}
 		
 		Optional<AuthenticationRequest> optionalAuthRequest = authService.parseAuthenticationRequest(request, response);
 		
-		// CASE 2:
-		// Token is present in the request, but it is not valid.
-		if (optionalAuthRequest.isEmpty()) {
-			logger.warn("Token is expired, malformed or the signature is invalid");
-			standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
+		// Non-public + parse token
+		boolean isTokenInvalid = optionalAuthRequest.isEmpty();
+		if (isTokenInvalid) {
+			if(isActionTokenEndpoint) {
+				standardResponseHandler.handleResponse(response, 401, "Access denied: invalid or missing token", null);
+				return;
+			}
+			logger.warn("Token is expired, malformed or the signature is invalid. Continues with Security Filter Chain...");
+			filterChain.doFilter(request, response);
 			return;
 		}
 		
-		// CASE 3:
-		// optionalAuthRequest.isPresent() is present, everything is OK, and we can continue with authentication process.
+		// Route by requestPath (valid token)
 		AuthenticationRequest authRequest = optionalAuthRequest.get();
-		String endpointPath = authRequest.getPath();
 		String method = authRequest.getMethod();
 		String token = authRequest.getToken();
 		String path = authRequest.getPath();
@@ -73,34 +120,40 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 		TokenType purpose = authRequest.getPurpose();
 
 		try {
-			switch(endpointPath) {
+			switch(path) {
 			
-			// TokenType.REFRESH
 			case Constants.LOGOUT_PATH:  
 				authFlowHandler.handleLogOutFlow(response, token, purpose, method);
-				break;
+			    if (response.isCommitted()) {
+			        return; 
+			    }
+			    break;
 
-			// TokenType.VERIFICATION
 			case Constants.REGISTRATION_PATH:
 			case Constants.RESET_PASSWORD_PATH:
-				if(authFlowHandler.handleVerificationFlow(request, response, token, purpose, method, path)) {
-					filterChain.doFilter(request, response);
+				authFlowHandler.handleVerificationFlow(request, response, token, purpose, method, username, path);
+				if (response.isCommitted()) {
+					return; 
 				}
-				break;
-
-			// TokenType.REFRESH
+				filterChain.doFilter(request, response);
+				return;
+			
 			case Constants.REFRESH_TOKEN_PATH:
-				if(authFlowHandler.handleRefreshFlow(request, response, token, purpose, username, method, path)) {
-					filterChain.doFilter(request, response);
+				authFlowHandler.handleRefreshFlow(request, response, token, purpose, username, path);
+				if (response.isCommitted()) {
+					return;  
 				}
-				break;
-
-			// TokenType.ACCESS
+				// Just in case there is no answer.
+			    filterChain.doFilter(request, response);
+			    return;
+				
 			default:
-				if(authFlowHandler.handleAccessFlow(response, token, purpose, username)) {
-					filterChain.doFilter(request, response);
+				authFlowHandler.handleAccessFlow(response, token, purpose, username);
+				if (response.isCommitted()) {
+					return;  
 				}
-				break;
+				filterChain.doFilter(request, response);
+			    return;
 			}
 
 		} catch (IOException ex) {
