f00de022c294d6bd8d279f6b2f4e93363d5b3a1c
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-09-19 12:50:33 +0200

    Fix(test): createUser() creates and returns User entity, without persistence; add returnUserAsJson()

--- src/test/java/dev/jcasaslopez/user/testhelper/TestHelper.java
+++ src/test/java/dev/jcasaslopez/user/testhelper/TestHelper.java
@@ -11,6 +11,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import dev.jcasaslopez.user.entity.Role;
 import dev.jcasaslopez.user.entity.User;
@@ -34,51 +39,41 @@ public class TestHelper {
 	@Autowired private LoginAttemptRepository loginAttemptRepository;
 	@Autowired private RedisTemplate<String, String> redisTemplate;
 	@Autowired private PasswordEncoder passwordEncoder;
+	@Autowired private ObjectMapper mapper;
 	
-	public User createUser(String username, String password) {
-		
-		// Prepares the table 'roles' in the DB by persisting all possible roles.
-		Role userRole = new Role(RoleName.ROLE_USER);
-		roleRepository.save(userRole);
-		Role adminRole = new Role(RoleName.ROLE_ADMIN);
-		roleRepository.save(adminRole);
-		Role superAdminRole = new Role(RoleName.ROLE_SUPERADMIN);
-		roleRepository.save(superAdminRole);
-		
-		User user = new User (	username, 
-								password, 
-								"Jorge García", 
-								"jorgecasas22@hotmail.com", 
-								LocalDate.of(1978, 11, 26)
-							  );
-		
-		// It would be less cumbersome to use UserDetailsManagerImpl.createUser(), but this method 
-		// does not return the user.
-		Set<Role> roles = new HashSet<>();
-		roles.add(userRole);
-		
-		user.setRoles(roles);
-		user.setAccountStatus(AccountStatus.ACTIVE);
-		user.setPassword(passwordEncoder.encode("Jorge22!"));	
-		
-		userRepository.save(user);
-		userRepository.flush();
-		return user;
+    @Transactional
+	public String returnUserAsJson(User user) throws JsonProcessingException {		
+		// Jackson cannot serialize or deserialize java.time.LocalDate by default
+		mapper.registerModule(new JavaTimeModule());
+
+		return mapper.writeValueAsString(user);
 	}
+	
+    // It would be more convenient to use UserDetailsManagerImpl.createUser(), but this method does not return the user
+    public User createUser(String username, String password) {
+    	User user = new User (username, password, "Jorge García", "jorgecasas22@hotmail.com", LocalDate.of(1978, 11, 26));
+
+    	Set<Role> roles = new HashSet<>();
+    	Role userRole = new Role (RoleName.ROLE_USER);
+    	roles.add(userRole);
+
+    	user.setRoles(roles);
+    	user.setAccountStatus(AccountStatus.ACTIVE);
+    	return user;
+    }
 	
 	@SuppressWarnings("deprecation")
 	public void cleanDataBaseAndRedis() {
 		loginAttemptRepository.deleteAll();
         userRepository.deleteAll();
-        roleRepository.deleteAll();
         redisTemplate.getConnectionFactory().getConnection().flushAll();
 	}
 	
 	public String loginUser(User userJpa, TokenType tokenType) {
 		CustomUserDetails user = userMapper.userToCustomUserDetailsMapper(userJpa);
-    	Authentication authentication = new UsernamePasswordAuthenticationToken
-										(user, user.getPassword(), user.getAuthorities());
+		String username = user.getUsername();
+		Authentication authentication = new UsernamePasswordAuthenticationToken (user, user.getPassword(), user.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-		return tokenService.createAuthToken(tokenType);
+		return tokenService.createAuthToken(tokenType, username);
 	}
 }
