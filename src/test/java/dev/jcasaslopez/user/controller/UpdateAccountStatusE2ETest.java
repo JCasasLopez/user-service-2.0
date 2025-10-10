4bb5cbc76420f6eabe47efcf0af32db0d76abc75
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-10-10 17:35:48 +0200

    Test: refactor 'update account status' endpoint tests to make them independant of one another, move security test to another class and rename class

--- src/test/java/dev/jcasaslopez/user/controller/UpdateAccountStatusE2ETest.java
+++ src/test/java/dev/jcasaslopez/user/controller/UpdateAccountStatusE2ETest.java
@@ -2,18 +2,13 @@ package dev.jcasaslopez.user.controller;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -46,9 +41,7 @@ import dev.jcasaslopez.user.utilities.Constants;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class UpdateAccountStatusIntegrationTest {
+public class UpdateAccountStatusE2ETest {
 	
 	@Autowired private UserRepository userRepository;
 	@Autowired private TestHelper testHelper; 
@@ -56,76 +49,41 @@ public class UpdateAccountStatusIntegrationTest {
 	@Autowired private MockMvc mockMvc;
 	@Autowired private ObjectMapper objectMapper;
 	
-	// This test defines 3 different users:
-	// 1) Target user: The account subject to modification. Shared across tests.
-	private static User user;
-	private static final String USERNAME = "Yorch22";
-	private static final String ORIGINAL_PASSWORD = "Password123!";
-
-	// 2) Plain user: Attempts modification (expected to fail - 403 Forbidden).
-	private static final String PLAIN_USER_USERNAME = "plainUser";
-	private static final String PLAIN_USER_PASSWORD = "Password456!";
+	private User user;
+	private String authToken;
 	
-	// 3) Admin user: Performs successful modification (expected to succeed - 200 OK).
 	private static final String ADMIN_USER_USERNAME = "userAdmin";
-	private static final String ADMIN_USER_PASSWORD = "Password789!";
+	private static final String ADMIN_USER_PASSWORD = "Password456!";
 	
-	// Static variable to share state between the ordered test methods.
-	private static String authToken;
-
-	@BeforeAll
-	void setup() {
-		UserTestBuilder builderUser = new UserTestBuilder(USERNAME, ORIGINAL_PASSWORD);
+	@BeforeEach
+	void setUp() throws Exception {
+		// This test defines 2 different users:
+		// 1) Target user: The account subject to modification. 
+		UserTestBuilder builderUser = new UserTestBuilder("Yorch22", "Password123!");
 		user = testHelper.createAndPersistUser(builderUser);
 		
-		UserTestBuilder builderPlainUser = new UserTestBuilder(PLAIN_USER_USERNAME, PLAIN_USER_PASSWORD);
-		testHelper.createAndPersistUser(builderPlainUser);
+		// 2) Admin user: Performs successful modification (expected to succeed - 200 OK).
+		UserTestBuilder builderAdminUser = new UserTestBuilder(ADMIN_USER_USERNAME, ADMIN_USER_PASSWORD).withRole(RoleName.ROLE_ADMIN);
+		testHelper.createAndPersistUser(builderAdminUser);	
 		
-		UserTestBuilder builderAdminUser = new UserTestBuilder(ADMIN_USER_USERNAME, ADMIN_USER_PASSWORD)
-														.withRole(RoleName.ROLE_ADMIN);
-		testHelper.createAndPersistUser(builderAdminUser);
+		authToken = authTestHelper.logInWithMockMvc(ADMIN_USER_USERNAME, ADMIN_USER_PASSWORD).getAccessToken();
 	}
 	
-	@AfterAll
+	@AfterEach
 	void cleanup() {
 	    testHelper.cleanDataBaseAndRedis();
 	}
 	
 	@Test
-	@Order(1)
-	@DisplayName("Plain user cannot update account status")
-	public void updateAccountStatus_WhenUserNotAdmin_ShouldReturn403Forbidden() throws Exception {
-		// Arrange
-		authToken = authTestHelper.logInWithMockMvc(PLAIN_USER_USERNAME, PLAIN_USER_PASSWORD).getAccessToken();
-		AccountStatus newAccountStatus = AccountStatus.TEMPORARILY_BLOCKED;
-		RequestBuilder requestBuilder = buildMockMvcRequest(newAccountStatus, authToken);
-
-		// Act
-		MvcResult mvcResult = callEndpointAndUReloadUser(requestBuilder);
-
-		String responseAsString = mvcResult.getResponse().getContentAsString();
-		StandardResponse response = objectMapper.readValue(responseAsString, StandardResponse.class);
-
-		// Assert
-		assertAll(
-		    () -> assertEquals(403, mvcResult.getResponse().getStatus(), "HTTP status should be 403 Forbidden"),
-		    () -> assertNotNull(response.getMessage(), "Response body should not be null"),
-		    () -> assertTrue(response.getMessage().contains("Access denied"), "Unexpected response message"),
-		    () -> assertFalse(user.getAccountStatus() == newAccountStatus, "Account status is " + user.getAccountStatus() + " but should be " + AccountStatus.PERMANENTLY_SUSPENDED)
-		);
-	}
-	
-	@Test
-	@Order(2)
 	@DisplayName("Admin updates account status successfully")
 	public void updateAccountStatus_WhenUserAdmin_ShouldReturn200Ok() throws Exception{
-		// Arrange
-		authToken = authTestHelper.logInWithMockMvc(ADMIN_USER_USERNAME, ADMIN_USER_PASSWORD).getAccessToken();
+		// Arrange		
 		AccountStatus newAccountStatus = AccountStatus.TEMPORARILY_BLOCKED;
 		RequestBuilder requestBuilder = buildMockMvcRequest(newAccountStatus, authToken);
 
 		// Act
-		MvcResult mvcResult = callEndpointAndUReloadUser(requestBuilder);
+		MvcResult mvcResult = callEndpoint(requestBuilder);
+	    user = reloadUser();
 
 		String responseAsString = mvcResult.getResponse().getContentAsString();
 		StandardResponse response = objectMapper.readValue(responseAsString, StandardResponse.class);
@@ -140,16 +98,16 @@ public class UpdateAccountStatusIntegrationTest {
 	}
 	
 	@Test
-	@Order(3)
 	@DisplayName("Throws exception when trying to update to the same status")
 	public void updateAccountStatus_WhenNewStatusIsSame_ShouldThrowException() throws Exception{
-		// Arrange
-		// Admin user already logged in. We re-use the token received then.
-		AccountStatus newAccountStatus = AccountStatus.TEMPORARILY_BLOCKED;
+		// Arrange		
+		// The default account status is ACTIVE, so trying to update it to the same status, should throw an exception.
+		AccountStatus newAccountStatus = AccountStatus.ACTIVE;
 		RequestBuilder requestBuilder = buildMockMvcRequest(newAccountStatus, authToken);
 
 		// Act
-		MvcResult mvcResult = callEndpointAndUReloadUser(requestBuilder);
+		MvcResult mvcResult = callEndpoint(requestBuilder);
+	    user = reloadUser();
 
 		String responseAsString = mvcResult.getResponse().getContentAsString();
 		StandardResponse response = objectMapper.readValue(responseAsString, StandardResponse.class);
@@ -163,21 +121,18 @@ public class UpdateAccountStatusIntegrationTest {
 	}
 	
 	@Test
-	@Order(4)
 	@DisplayName("Throws exception when trying to update a permanently suspended account")
 	public void updateAccountStatus_WhenAccountIsPermanentlySuspended_ShouldThrowException() throws Exception{
 		// Arrange
-		// Admin user already logged in. We re-use the token received then.
-		// Set the account status to 'PERMANENTLY SUSPENDED' using the endpoint to decouple base code and tests.
 		AccountStatus newAccountStatus = AccountStatus.PERMANENTLY_SUSPENDED;
 		RequestBuilder requestBuilder = buildMockMvcRequest(newAccountStatus, authToken);
-		callEndpointAndUReloadUser(requestBuilder);
+		callEndpoint(requestBuilder);
 
 		// Act
 		// Update the account to 'ACTIVE'. The result of this call is the test real subject.
 		AccountStatus accountStatusSecondCall = AccountStatus.ACTIVE;
 		RequestBuilder requestBuilderSecondCall = buildMockMvcRequest(accountStatusSecondCall, authToken);
-		MvcResult mvcResultSecondCall = callEndpointAndUReloadUser(requestBuilderSecondCall);
+		MvcResult mvcResultSecondCall = callEndpoint(requestBuilderSecondCall);
 
 		String responseAsString = mvcResultSecondCall.getResponse().getContentAsString();
 		StandardResponse response = objectMapper.readValue(responseAsString, StandardResponse.class);
@@ -191,13 +146,11 @@ public class UpdateAccountStatusIntegrationTest {
 	}
 	
 	// ************** HELPER METHODS **************
-	// These three helper methods are shared by UpdateAccountStatusIntegrationTest and UpgradeUserIntegrationTest,
-	// the second one is identical in both cases, the first one requires only slight modifications. However,
+	// These helper methods are shared by UpdateAccountStatusIntegrationTest and UpgradeUserIntegrationTest, however,
 	// attempts to take them to a helper class to avoid code repetition render the code way more complex and difficult
 	// to follow, so the decision to keep the code repeated is a conscious trade-off.
 		
 	private RequestBuilder buildMockMvcRequest(AccountStatus newAccountStatus, String authToken) throws JsonProcessingException {
-				
 		return MockMvcRequestBuilders
 		        .put(Constants.UPDATE_ACCOUNT_STATUS_PATH)
 				.header(HttpHeaders.AUTHORIZATION, "Bearer " + authToken)
@@ -207,13 +160,12 @@ public class UpdateAccountStatusIntegrationTest {
 		        .accept(MediaType.APPLICATION_JSON);
 	}
 
-	// Performs the request and refreshes user from the database, simulating a full controller-to-database roundtrip.
-	private MvcResult callEndpointAndUReloadUser(RequestBuilder requestBuilder) throws Exception {
-		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+	private MvcResult callEndpoint(RequestBuilder requestBuilder) throws Exception {
+	    return mockMvc.perform(requestBuilder).andReturn();
+	}
 
-		// Reloads user from the database with the upgraded roles.
-		user = userRepository.findById(user.getIdUser()).orElseThrow(
-				() -> new UsernameNotFoundException(user.getUsername() + " was not found in the database"));
-		return mvcResult;
+	private User reloadUser() {
+	    return userRepository.findById(user.getIdUser())
+	        .orElseThrow(() -> new UsernameNotFoundException(user.getUsername() + " was not found in the database"));
 	}
 }
