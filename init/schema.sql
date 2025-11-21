f0b6f9afb38085e66bd37a6685cad1a557289c83
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-11-21 19:07:49 +0100

    Fix: update schema.sql to reflect changes in database

--- init/schema.sql
+++ init/schema.sql
@@ -1,32 +1,40 @@
 CREATE TABLE IF NOT EXISTS roles (
-    idRole INT AUTO_INCREMENT PRIMARY KEY,
-    roleName VARCHAR(50) UNIQUE NOT NULL
+    idRole INT NOT NULL AUTO_INCREMENT,
+    roleName ENUM('ROLE_USER','ROLE_ADMIN','ROLE_SUPERADMIN') NOT NULL, 
+    PRIMARY KEY (idRole),
+    UNIQUE KEY roleName_UNIQUE (roleName) 
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    idUser INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(20) UNIQUE NOT NULL,
+    idUser INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    fullName VARCHAR(30),
-    email VARCHAR(255) UNIQUE NOT NULL,
-    dateOfBirth DATE,
-    accountStatus VARCHAR(50)
+    fullName VARCHAR(30) NOT NULL, 
+    email VARCHAR(45) NOT NULL, 
+    dateOfBirth DATE NOT NULL, 
+    accountStatus ENUM('ACTIVE','TEMPORARILY_BLOCKED','BLOCKED','PERMANENTLY_SUSPENDED') NOT NULL,
+    PRIMARY KEY (idUser),
+    UNIQUE KEY username_UNIQUE (username), 
+    UNIQUE KEY email_UNIQUE (email)
 );
 
 CREATE TABLE IF NOT EXISTS user_roles (
-    user_id INT,
-    role_id INT,
+    user_id INT NOT NULL, 
+    role_id INT NOT NULL, 
     PRIMARY KEY (user_id, role_id),
+
     FOREIGN KEY (user_id) REFERENCES users(idUser) ON DELETE CASCADE,
     FOREIGN KEY (role_id) REFERENCES roles(idRole)
 );
 
 CREATE TABLE IF NOT EXISTS login_attempts (
-    idLoginAttempt BIGINT AUTO_INCREMENT PRIMARY KEY,
-    timestamp DATETIME(6),
-    successful BIT NOT NULL,
-    ipAddress VARCHAR(255),
-    loginFailureReason TINYINT CHECK (loginFailureReason BETWEEN 0 AND 4),
-    idUser INT NULL,
-    FOREIGN KEY (idUser) REFERENCES users(idUser) ON DELETE SET NULL
+    idLoginAttempt BIGINT NOT NULL AUTO_INCREMENT, 
+    timestamp DATETIME NOT NULL, 
+    successful BIT(1) DEFAULT NULL, 
+    ipAddress VARCHAR(45) NOT NULL, 
+    loginFailureReason ENUM('INCORRECT_PASSWORD','USER_NOT_FOUND','MISSING_FIELD','ACCOUNT_LOCKED','OTHER') DEFAULT NULL, -- << CORREGIDO: ENUM
+    idUser INT DEFAULT NULL,
+    PRIMARY KEY (idLoginAttempt),
+    KEY idUser_idx (idUser), 
+    CONSTRAINT idUser FOREIGN KEY (idUser) REFERENCES users(idUser) ON DELETE SET NULL 
 );
