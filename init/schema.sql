3d2f03248d0e5cd58771a90e6fce85097d234479
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-12-01 12:52:22 +0100

    Fix(schema.sql): modify tables fields to reflect real database design

--- init/schema.sql
+++ init/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(20) NOT NULL,
     password VARCHAR(255) NOT NULL,
     fullName VARCHAR(30) NOT NULL, 
-    email VARCHAR(45) NOT NULL, 
+    email VARCHAR(255) NOT NULL, 
     dateOfBirth DATE NOT NULL, 
     accountStatus ENUM('ACTIVE','TEMPORARILY_BLOCKED','BLOCKED','PERMANENTLY_SUSPENDED') NOT NULL,
     PRIMARY KEY (idUser),
@@ -22,19 +22,18 @@ CREATE TABLE IF NOT EXISTS user_roles (
     user_id INT NOT NULL, 
     role_id INT NOT NULL, 
     PRIMARY KEY (user_id, role_id),
-
     FOREIGN KEY (user_id) REFERENCES users(idUser) ON DELETE CASCADE,
     FOREIGN KEY (role_id) REFERENCES roles(idRole)
 );
 
 CREATE TABLE IF NOT EXISTS login_attempts (
     idLoginAttempt BIGINT NOT NULL AUTO_INCREMENT, 
-    timestamp DATETIME NOT NULL, 
-    successful BIT(1) DEFAULT NULL, 
+    timestamp DATETIME(6) NOT NULL, 
+    successful BIT(1) NOT NULL, 
     ipAddress VARCHAR(45) NOT NULL, 
-    loginFailureReason ENUM('INCORRECT_PASSWORD','USER_NOT_FOUND','MISSING_FIELD','ACCOUNT_LOCKED','OTHER') DEFAULT NULL, -- << CORREGIDO: ENUM
+    loginFailureReason ENUM('INCORRECT_PASSWORD','USER_NOT_FOUND','MISSING_FIELD','ACCOUNT_LOCKED','OTHER') DEFAULT NULL,
     idUser INT DEFAULT NULL,
     PRIMARY KEY (idLoginAttempt),
     KEY idUser_idx (idUser), 
-    CONSTRAINT idUser FOREIGN KEY (idUser) REFERENCES users(idUser) ON DELETE SET NULL 
+    CONSTRAINT fk_idUser FOREIGN KEY (idUser) REFERENCES users(idUser) ON DELETE SET NULL 
 );
